d2cc21db87127b313087b96c479a7c5380faa7c9
Prshant Panchal <ppa@odoo.com>
2018-03-20 13:02:32 -0700

    [FIX] api.constrains isntead of create

--- sales_customization_mexytul/models/sale.py
+++ sales_customization_mexytul/models/sale.py
@@ -11,20 +11,20 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         if not self.user_has_groups("sales_customization_mexytul.group_mexytul_credit_limit"):
             for order in self:
-                amount_total = sum(self.search([('partner_id', '=', order.partner_id.id), ('state', '=', 'sale')]).mapped('amount_total'))
-                if (order.partner_id.credit_limit - amount_total) < order.amount_total:
+                amount_total = sum(self.search([('partner_id', '=', order.partner_id.id), ('state', '=', 'sale'), ('invoice_status', '=', 'to invoice')].mapped('amount_total'))
+                if (order.partner_id.credit_limit - amount_total) < order.amount_total):
                     raise Warning(_("The Selected customer exceeds the credit limit set or have amount due"))
                 if order.order_line.filtered(lambda ol: ol.price_unit < (ol.product_id.base_price or 1)):
                     raise Warning(_("The Unit price of one or more product is Zero"))
         return super(SaleOrder, self).action_confirm()
 
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.model
-    def create(self, values):
-        if not self.user_has_groups("sales_customization_mexytul.group_mexytul_credit_limit"):
-            product_id = self.env['product.product'].browse(int(values['product_id']))
-            if values['price_unit'] < (product_id.base_price or 1):
-                raise Warning(_("The Unit Price is below the base price"))
-        return super(SaleOrderLine, self).create(values)
+    @api.multi
+    @api.constrains('price_unit', 'base_price')
+    def validate_prices(self):
+        if not self.user_has_groups("sales_customization_mexytul.group_mexytul_credit_limit") and\
+            self.filtered(lambda ol: ol.price_unit < ol.product_id.base_price:):
+            raise Warning(_("The Unit Price is below the base price"))
