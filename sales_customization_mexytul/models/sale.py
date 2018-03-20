cf55161eabe46605bcc54dd27e0da8a65b94d986
Jigar Amin (Odoo) <jam@odoo.com>
2018-03-20 13:08:01 -0700

    [FIX] lint errors

--- sales_customization_mexytul/models/sale.py
+++ sales_customization_mexytul/models/sale.py
@@ -11,8 +11,8 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         if not self.user_has_groups("sales_customization_mexytul.group_mexytul_credit_limit"):
             for order in self:
-                amount_total = sum(self.search([('partner_id', '=', order.partner_id.id), ('state', '=', 'sale'), ('invoice_status', '=', 'to invoice')].mapped('amount_total'))
-                if (order.partner_id.credit_limit - amount_total) < order.amount_total):
+                amount_total = sum(self.search([('partner_id', '=', order.partner_id.id), ('state', '=', 'sale'), ('invoice_status', '=', 'to invoice')].mapped('amount_total')))
+                if (order.partner_id.credit_limit - amount_total) < order.amount_total:
                     raise Warning(_("The Selected customer exceeds the credit limit set or have amount due"))
                 if order.order_line.filtered(lambda ol: ol.price_unit < (ol.product_id.base_price or 1)):
                     raise Warning(_("The Unit price of one or more product is Zero"))
@@ -25,6 +25,5 @@ class SaleOrderLine(models.Model):
     @api.multi
     @api.constrains('price_unit', 'base_price')
     def validate_prices(self):
-        if not self.user_has_groups("sales_customization_mexytul.group_mexytul_credit_limit") and\
-            self.filtered(lambda ol: ol.price_unit < ol.product_id.base_price:):
+        if not self.user_has_groups("sales_customization_mexytul.group_mexytul_credit_limit") and self.filtered(lambda ol: ol.price_unit < ol.product_id.base_price):
             raise Warning(_("The Unit Price is below the base price"))
