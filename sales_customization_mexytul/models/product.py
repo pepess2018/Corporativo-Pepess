164a940e751469efd6c8449d6eedf2de327c1baa
PRASHANT PANCHAL <ppa@odoo.com>
2018-03-23 16:26:23 +0530

    [IMP] the Sale Base Price calculated automatically. based on (%margin*cost_price)+cost_price and removed from product view

--- sales_customization_mexytul/models/product.py
+++ sales_customization_mexytul/models/product.py
@@ -8,40 +8,39 @@ from odoo.addons import decimal_precision as dp
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    margin = fields.Float(string='Margin %', help="Product base price = Margin'%'*cost price")
+    margin = fields.Float(string='Margin %', related='categ_id.margin', help="Product base price = Margin'%'*cost price")
     base_price = fields.Monetary(string='Sale Base Price', compute='_compute_product_base_price',
-                                                        digits=dp.get_precision('Product Price'), store=True)
+                                                        digits=dp.get_precision('Product Price'))
 
     @api.depends('margin', 'standard_price')
     def _compute_product_base_price(self):
         for product in self:
             product.base_price = product.standard_price + (product.standard_price*product.margin/100) or product.lst_price
 
-    @api.onchange('categ_id')
-    def onchange_categ_id(self):
-        if self.categ_id:
-            self.margin = self.categ_id.margin
+    # @api.onchange('categ_id')
+    # def onchange_categ_id(self):
+    #     if self.categ_id:
+    #         self.margin = self.categ_id.margin
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    def _default_margin(self):
-        return self.categ_id and self.categ_id.margin or self.product_tmpl_id and self.product_tmpl_id.margin
+    # def _default_margin(self):
+    #     return self.product_tmpl_id and self.product_tmpl_id.margin
 
-    margin = fields.Float(string='Margin %', help="Product base price = Margin'%'*cost price", default=_default_margin)
+    margin = fields.Float(string='Margin %', related='categ_id.margin', help="Product base price = Margin'%'*cost price")
     base_price = fields.Monetary(string='Sale Base Price', compute='_compute_product_base_price',
-                                                        digits=dp.get_precision('Product Price'), store=True)
+                                                        digits=dp.get_precision('Product Price'))
 
     @api.depends('margin', 'standard_price')
     def _compute_product_base_price(self):
         for product in self:
-            margin = not product.product_tmpl_id and product.margin or product.product_tmpl_id.margin
-            product.base_price = product.standard_price + (product.standard_price*margin/100) or product.list_price
+            product.base_price = product.standard_price + (product.standard_price*product.margin/100) or product.list_price
 
-    @api.onchange('categ_id')
-    def onchange_categ_id(self):
-        if self.categ_id:
-            self.margin = self.categ_id.margin
+    # @api.onchange('categ_id')
+    # def onchange_categ_id(self):
+    #     if self.categ_id:
+    #         self.margin = self.categ_id.margin
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
