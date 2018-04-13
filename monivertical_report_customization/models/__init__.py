a97d5a717634c0272b99e35689878e9fb7798c98
Jigar Amin (Odoo) <jam@odoo.com>
2018-04-13 15:17:09 -0700

    [DEL] removed unrelated module

--- monivertical_report_customization/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import res_company
-from . import res_partner
-from . import sale_order
-from . import stock_picking
-from . import stock_picking_driver
-from . import delivery_carrier
