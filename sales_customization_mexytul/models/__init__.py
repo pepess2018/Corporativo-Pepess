03b7ea9773223b70c246e6b7ca3a38aae95d7ea7
Jigar <jam-odoo@users.noreply.github.com>
2018-06-05 12:37:36 -0700

    [FIX] Load sequence

--- sales_customization_mexytul/models/__init__.py
+++ sales_customization_mexytul/models/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+from . import partner
 from . import product
 from . import sale
 from . import stock
-from . import partner
 from . import account
