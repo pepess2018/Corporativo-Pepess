f170fcd8c28f3776fc15b6ba6e68447711406382
Prashant Panchal <ppa@odoo.com>
2020-03-13 16:03:23 +0530

    [MIG] account_followup_report_maxytul: account followup reports

--- account_followup_report_maxytul/reports/account_followup_report.py
+++ account_followup_report_maxytul/reports/account_followup_report.py
@@ -2,16 +2,19 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime
-from odoo import api, fields, models, _
-from odoo.tools import float_round
+from odoo import fields, models, _
 from odoo.tools.misc import formatLang, format_date
 
 
 class AccountFollowupReport(models.AbstractModel):
     _inherit = "account.followup.report"
 
-    def get_columns_name(self, options):
-        headers = super(AccountFollowupReport, self).get_columns_name(options)
+    def _get_columns_name(self, options):
+        """
+            Override
+            Return the name of the columns of the follow-ups report
+        """
+        headers = super(AccountFollowupReport, self)._get_columns_name(options)
         headers.extend([
             {'name': _(' Dias Vencidos '), 'class': 'number', 'style': 'text-align:right; white-space:nowrap;'},
             {'name': _(' Dias De Credito '), 'style': 'text-align:right; white-space:nowrap;'},
@@ -23,7 +26,11 @@ class AccountFollowupReport(models.AbstractModel):
             ])
         return headers
 
-    def get_lines(self, options, line_id=None):
+    def _get_lines(self, options, line_id=None):
+        """
+            Override
+            Compute and return the lines of the columns of the follow-ups report.
+        """
         # Get date format for the lang
         partner = options.get('partner_id') and self.env['res.partner'].browse(options['partner_id']) or False
         if not partner:
@@ -32,21 +39,21 @@ class AccountFollowupReport(models.AbstractModel):
 
         lines = []
         res = {}
-        today = datetime.today().strftime('%Y-%m-%d')
+        today = fields.Date.today()
         line_num = 0
         for l in partner.unreconciled_aml_ids:
-            if self.env.context.get('print_mode') and l.blocked:
-                continue
-            currency = l.currency_id or l.company_id.currency_id
-            if currency not in res:
-                res[currency] = []
-            res[currency].append(l)
+            if l.company_id == self.env.user.company_id:
+                if self.env.context.get('print_mode') and l.blocked:
+                    continue
+                currency = l.currency_id or l.company_id.currency_id
+                if currency not in res:
+                    res[currency] = []
+                res[currency].append(l)
         for currency, aml_recs in res.items():
             total = 0
             total_issued = 0
-            aml_recs = sorted(aml_recs, key=lambda aml: aml.blocked)
             for aml in aml_recs:
-                amount = aml.currency_id and aml.amount_residual_currency or aml.amount_residual
+                amount = aml.amount_residual_currency if aml.currency_id else aml.amount_residual
                 date_due = format_date(self.env, aml.date_maturity or aml.date, lang_code=lang_code)
                 total += not aml.blocked and amount or 0
                 is_overdue = today > aml.date_maturity if aml.date_maturity else today > aml.date
@@ -67,16 +74,17 @@ class AccountFollowupReport(models.AbstractModel):
                 total_invoice = sum([inv_line.quantity * inv_line.price_unit for inv_line in aml.invoice_id.invoice_line_ids])
                 total_invoice = total_invoice + aml.invoice_id.amount_tax
                 # total discount
-                total_discount = sum([inv_line.discount and inv_line.price_unit*(inv_line.discount/100) * inv_line.quantity or 0.00 for inv_line in aml.invoice_id.invoice_line_ids])
+                total_discount = sum([inv_line.discount and inv_line.price_unit * (inv_line.discount / 100) * inv_line.quantity or 0.00 for inv_line in aml.invoice_id.invoice_line_ids])
                 payments = aml.invoice_id.payment_move_line_ids and sum([payment_vals['amount'] for payment_vals in aml.invoice_id._get_payments_vals()]) or 0.00
                 columns = [
                     format_date(self.env, aml.date, lang_code=lang_code),
                     date_due,
+                    aml.invoice_id.origin,
                     aml.invoice_id.name or aml.name,
                     aml.expected_pay_date and aml.expected_pay_date + ' ' + aml.internal_note or '',
                     {'name': aml.blocked, 'blocked': aml.blocked},
                     amount,
-                    aml.invoice_id and (datetime.strptime(aml.date_maturity, "%Y-%m-%d") - datetime.strptime(aml.invoice_id.date_invoice, "%Y-%m-%d")).days or 0,
+                    aml.invoice_id and (aml.date_maturity - aml.invoice_id.date_invoice).days or 0,
                     aml.invoice_id and aml.invoice_id.payment_term_id and aml.invoice_id.payment_term_id.display_name or '',
                     pricelist,
                     self.format_value(total_discount, currency=currency),
@@ -85,17 +93,21 @@ class AccountFollowupReport(models.AbstractModel):
                     amount
                     ]
                 if self.env.context.get('print_mode'):
-                    columns = columns[:3]+columns[5:]
+                    columns = columns[:4] + columns[6:]
                 lines.append({
                     'id': aml.id,
+                    'invoice_id': aml.invoice_id.id,
+                    'view_invoice_id': self.env['ir.model.data'].get_object_reference('account', 'invoice_form')[1],
+                    'account_move': aml.move_id,
                     'name': aml.move_id.name,
                     'caret_options': 'followup',
                     'move_id': aml.move_id.id,
                     'type': is_payment and 'payment' or 'unreconciled_aml',
                     'unfoldable': False,
+                    'has_invoice': bool(aml.invoice_id),
                     'columns': [type(v) == dict and v or {'name': v} for v in columns],
                 })
-            totalXXX = formatLang(self.env, total, currency_obj=currency)
+            total_due = formatLang(self.env, total, currency_obj=currency)
             line_num += 1
             lines.append({
                 'id': line_num,
@@ -103,7 +115,7 @@ class AccountFollowupReport(models.AbstractModel):
                 'class': 'total',
                 'unfoldable': False,
                 'level': 0,
-                'columns': [{'name': v} for v in ['']*(9 if self.env.context.get('print_mode') else 11) + [total >= 0 and _('Total Due') or '', totalXXX]],
+                'columns': [{'name': v} for v in [''] * (10 if self.env.context.get('print_mode') else 12) + [total >= 0 and _('Total Due') or '', total_due]],
             })
             if total_issued > 0:
                 total_issued = formatLang(self.env, total_issued, currency_obj=currency)
@@ -114,6 +126,19 @@ class AccountFollowupReport(models.AbstractModel):
                     'class': 'total',
                     'unfoldable': False,
                     'level': 0,
-                    'columns': [{'name': v} for v in ['']*(9 if self.env.context.get('print_mode') else 11) + [_('Total Overdue'), total_issued]],
+                    'columns': [{'name': v} for v in [''] * (10 if self.env.context.get('print_mode') else 12) + [_('Total Overdue'), total_issued]],
                 })
+            # Add an empty line after the total to make a space between two currencies
+            line_num += 1
+            lines.append({
+                'id': line_num,
+                'name': '',
+                'class': '',
+                'unfoldable': False,
+                'level': 0,
+                'columns': [{} for col in columns],
+            })
+        # Remove the last empty line
+        if lines:
+            lines.pop()
         return lines
